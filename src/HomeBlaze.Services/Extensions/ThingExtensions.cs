f6e039f2e566923ae93ffbfbf635f98e568d1da5
Rico Suter <rico.suter@buhlergroup.com>
2023-07-20 15:53:34 +0200

    Add GetActualTitle

---
+++ src/HomeBlaze.Services/Extensions/ThingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using HomeBlaze.Abstractions;
+using HomeBlaze.Abstractions.Services;
+
+namespace HomeBlaze.Services.Extensions
+{
+    public static class ThingExtensions
+    {
+        public static string? GetActualTitle(this IThing thing, IThingManager thingManager)
+        {
+            return thingManager
+                .GetState(thing.Id, true)
+                .OrderBy(s => s.Value.Attribute?.Order ?? 0)
+                .GroupBy(p => p.Value.SourceThing)
+                .SelectMany(g => g)
+                .FirstOrDefault(g => g.Key == "Title")
+                .Value.Value as string ?? thing.Title;
+        }
+    }
+}
