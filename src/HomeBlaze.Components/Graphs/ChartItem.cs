8c19e6c6f087761f9ed0b78551488f374d4ad7f8
Rico Suter <mail@rsuter.com>
2023-08-16 10:33:27 +0200

    improve history

---
+++ src/HomeBlaze.Components/Graphs/ChartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeBlaze.Components.Graphs
+{
+    internal class ChartItem
+    {
+        public int Index { get; set; }
+
+        public DateTimeOffset DateTime { get; set; }
+
+        public DateTime Date => DateTime.DateTime;
+
+        public object? RawValue { get; set; }
+
+        public double Value => double
+            .TryParse(RawValue?.ToString() ?? "", out var number) ?
+                Math.Round(number, 2) :
+                0;
+    }
+}
---
+++ src/HomeBlaze.Components/Graphs/ChartSeriesUtilities.cs
@@ -0,0 +1,72 @@
+﻿using HomeBlaze.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeBlaze.Components.Graphs
+{
+    internal static class ChartSeriesUtilities
+    {
+        public static ChartItem[] CleanUp(PropertyState? state, ChartItem[] values, int numberOfHours)
+        {
+            try
+            {
+                var startDate = DateTimeOffset.Now.AddHours(numberOfHours * -1);
+                var endDate = DateTimeOffset.Now;
+                var duration = endDate - startDate;
+
+                if (state?.Property?.PropertyType.Type == typeof(decimal) ||
+                    state?.Property?.PropertyType.Type == typeof(double) ||
+                    state?.Property?.PropertyType.Type == typeof(float) ||
+                    state?.Property?.PropertyType.Type == typeof(long) ||
+                    state?.Property?.PropertyType.Type == typeof(int))
+                {
+                    var chartItems = new List<ChartItem>();
+
+                    if (values.Length > 720)
+                    {
+                        var numberOfValues = numberOfHours < 24 * 2 ? 24 * 2 : numberOfHours;
+                        var valueDuration = duration / numberOfValues;
+
+                        for (int i = 0; i < numberOfValues; i++)
+                        {
+                            var start = startDate + valueDuration * i;
+                            var end = startDate + valueDuration * (i + 1);
+
+                            var filteredValues = values
+                                .Where(v => v.DateTime >= start && v.DateTime < end)
+                                .ToArray();
+
+                            chartItems.Add(new ChartItem
+                            {
+                                Index = i,
+                                DateTime = start + valueDuration / 2,
+                                RawValue =
+                                    filteredValues.Any() ?
+                                    filteredValues
+                                        .Select(v => v.Value)
+                                        .Average() : null
+                            });
+                        }
+                    }
+                    else
+                    {
+                        chartItems = values.ToList();
+                    }
+
+                    return chartItems
+                        .Where(v => v.RawValue != null)
+                        .ToArray();
+                }
+                else
+                {
+                    return Array.Empty<ChartItem>();
+                }
+            }
+            catch
+            {
+                return Array.Empty<ChartItem>();
+            }
+        }
+    }
+}
