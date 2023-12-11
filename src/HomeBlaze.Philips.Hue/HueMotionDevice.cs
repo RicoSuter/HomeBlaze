922c9467c3d92b3d95332b62e3e347f778b427dd
Rico Suter <mail@rsuter.com>
2023-12-11 19:25:34 +0100

    Use new Hue API (#5)
    * Use new Hue API
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: Rico Suter <rico.suter@buhlergroup.com>

---
+++ src/HomeBlaze.Philips.Hue/HueMotionDevice.cs
@@ -0,0 +1,67 @@
+﻿using HomeBlaze.Abstractions;
+using HomeBlaze.Abstractions.Attributes;
+using HomeBlaze.Abstractions.Devices.Energy;
+using HomeBlaze.Abstractions.Presentation;
+using HomeBlaze.Abstractions.Sensors;
+using HueApi.Models;
+using HueApi.Models.Sensors;
+
+namespace HomeBlaze.Philips.Hue
+{
+    public class HueMotionDevice :
+        HueDevice,
+        IThing,
+        IIconProvider,
+        IPresenceSensor,
+        IBatteryDevice,
+        ILightSensor,
+        ITemperatureSensor
+    {
+        public string IconName => "fas fa-running";
+
+        [State]
+        public bool? IsPresent => MotionResource.Motion.MotionState;
+
+        [State]
+        public decimal? BatteryLevel => DevicePowerResource?.PowerState?.BatteryLevel / 100m;
+
+        internal MotionResource MotionResource { get; set; }
+
+        internal DevicePower? DevicePowerResource { get; set; }
+
+        internal TemperatureResource? TemperatureResource { get; set; }
+
+        internal LightLevel? LightLevelResource { get; set; }
+
+        [State]
+        public decimal? Temperature =>
+            TemperatureResource?.ExtensionData["temperature"].GetProperty("temperature_valid").GetBoolean() == true ?
+            TemperatureResource?.ExtensionData["temperature"].GetProperty("temperature").GetDecimal() : null;
+
+        [State]
+        public decimal? LightLevel => LightLevelResource?.Light.LightLevelValid == true ? (decimal?)LightLevelResource?.Light.LuxLevel : null;
+
+        public HueMotionDevice(Device device, ZigbeeConnectivity? zigbeeConnectivity, DevicePower? devicePower, TemperatureResource? temperature, LightLevel? lightLevel, MotionResource motion, HueBridge bridge)
+            : base(device, zigbeeConnectivity, bridge)
+        {
+            DevicePowerResource = devicePower;
+            TemperatureResource = temperature;
+            LightLevelResource = lightLevel;
+            MotionResource = motion;
+
+            Update(device, zigbeeConnectivity, devicePower, temperature, lightLevel, motion);
+        }
+
+        internal HueMotionDevice Update(Device device, ZigbeeConnectivity? zigbeeConnectivity, DevicePower? devicePower, TemperatureResource? temperature, LightLevel? lightLevel, MotionResource motion)
+        {
+            Update(device, zigbeeConnectivity);
+
+            DevicePowerResource = devicePower;
+            TemperatureResource = temperature;
+            LightLevelResource = lightLevel;
+            MotionResource = motion;
+
+            return this;
+        }
+    }
+}
