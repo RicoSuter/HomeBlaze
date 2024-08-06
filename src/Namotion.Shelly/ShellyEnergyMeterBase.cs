47751b26648088ae6dd50a0b4cc62b0b6a1566a1
Rico Suter <mail@rsuter.com>
2024-08-06 10:36:39 +0200

    up

--- src/Namotion.Shelly/ShellyEnergyMeterBase.cs
+++ src/Namotion.Shelly/ShellyEnergyMeterBase.cs
@@ -9,6 +9,63 @@ using System;
 
 namespace Namotion.Shelly
 {
+    public class EnergyData
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        [JsonPropertyName("id")]
+        public int Identifier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total active energy for phase A, measured in watts.
+        /// </summary>
+        [JsonPropertyName("a_total_act_energy")]
+        public double PhaseATotalActiveEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total active returned energy for phase A, measured in watts.
+        /// </summary>
+        [JsonPropertyName("a_total_act_ret_energy")]
+        public double PhaseATotalActiveReturnedEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total active energy for phase B, measured in watts.
+        /// </summary>
+        [JsonPropertyName("b_total_act_energy")]
+        public double PhaseBTotalActiveEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total active returned energy for phase B, measured in watts.
+        /// </summary>
+        [JsonPropertyName("b_total_act_ret_energy")]
+        public double PhaseBTotalActiveReturnedEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total active energy for phase C, measured in watts.
+        /// </summary>
+        [JsonPropertyName("c_total_act_energy")]
+        public double PhaseCTotalActiveEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total active returned energy for phase C, measured in watts.
+        /// </summary>
+        [JsonPropertyName("c_total_act_ret_energy")]
+        public double PhaseCTotalActiveReturnedEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total active energy across all phases, measured in watts.
+        /// </summary>
+        [JsonPropertyName("total_act")]
+        public double TotalActiveEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total active returned energy across all phases, measured in watts.
+        /// </summary>
+        [JsonPropertyName("total_act_ret")]
+        public double TotalActiveReturnedEnergy { get; set; }
+    }
+
     [GenerateProxy]
     public class ShellyEnergyMeterBase :
         IThing,
