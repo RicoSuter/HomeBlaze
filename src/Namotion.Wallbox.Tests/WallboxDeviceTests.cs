5d108aebe5d8d24377d14327a093e76b44a5107d
Rico Suter <mail@rsuter.com>
2024-08-09 11:11:28 +0200

    update test

--- src/Namotion.Wallbox.Tests/WallboxDeviceTests.cs
+++ src/Namotion.Wallbox.Tests/WallboxDeviceTests.cs
@@ -4,49 +4,13 @@ namespace Namotion.Shelly.Tests
 {
     public class WallboxDeviceTests
     {
-        private const string TestCoverIpAddress = "192.168.1.125";
-        private const string TestEmIpAddress = "192.168.1.133";
-
         [Fact(Skip = "No CI")]
         public async Task ShouldConnectToCover()
         {
-            var x = new WallboxClient(new HttpClient(), "", "");
-            var reso = await x.GetChargerStatusAsync("123456", default);
-            Assert.NotNull(reso);
-
-            //var shellyDevice = ShellyDevice.Create();
-            //shellyDevice.IpAddress = TestCoverIpAddress;
-
-            //await shellyDevice.StartAsync(CancellationToken.None);
-            //try
-            //{
-            //    await WaitForAsync(() => shellyDevice.IsConnected);
-
-            //    // Assert
-            //    Assert.True(shellyDevice.IsConnected);
-            //    Assert.NotNull(shellyDevice.Information);
-            //    Assert.NotNull(shellyDevice.Information.Version);
-
-            //    Assert.NotNull(shellyDevice.Cover);
-            //}
-            //finally
-            //{
-
-            //    await shellyDevice.StopAsync(CancellationToken.None);
-            //}
-        }
-
-        private static async Task WaitForAsync(Func<bool> condition)
-        {
-            for (var i = 0; i < 100; i++)
-            {
-                await Task.Delay(200);
-
-                if (condition())
-                {
-                    break;
-                }
-            }
+            var client = new WallboxClient(new HttpClient(), "email", "password");
+            var chargers = await client.GetChargersAsync(default);
+            var chargerStatus = await client.GetChargerStatusAsync(chargers.First().SerialNumber!, default);
+            Assert.NotNull(chargerStatus);
         }
     }
 }
